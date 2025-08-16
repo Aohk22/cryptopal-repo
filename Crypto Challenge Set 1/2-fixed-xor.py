f3dcc43adabac9a0530be09655df20dc4c37bddd
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-08-16 18:01:24 +0700

    2nd challenge

--- Crypto Challenge Set 1/2-fixed-xor.py
+++ Crypto Challenge Set 1/2-fixed-xor.py
@@ -7,10 +7,11 @@ def test():
     str1 = '1c0111001f010100061a024b53535009181c'
     str2 = '686974207468652062756c6c277320657965'
     result = '746865206b696420646f6e277420706c6179'
-    return fixed_xor(str1, str2).decode() == result
+    xor_result = fixed_xor(str1, str2).hex()
+    return xor_result == result
 
 def main():
-    test()
+    print(test())
 
 if __name__ == '__main__':
     main()
